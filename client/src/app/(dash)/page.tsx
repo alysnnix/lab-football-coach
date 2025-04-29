acf86d6b1628e1c742d7f4bf133b21b19d29812c
Alysson <4ly.alcantara@gmail.com>
2025-04-29 20:41:31 -0300

    feat(client): replace DataTable with UserList component
    - removed DataTable component from `page.tsx`
    - added `UserList` component wrapped in Suspense for loading state
    - cleaned up unused imports and code from `user-list/index.tsx`

--- client/src/app/(dash)/page.tsx
+++ client/src/app/(dash)/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import { StatusBar } from "./status-bar";
-import { DataTable } from "./user-list";
+import { UserList } from "./user-list";
 
 export default function Home() {
   return (
     <main className="bg-slate-200">
       <StatusBar />
-      <DataTable data={[]} />
+      <Suspense fallback={<p>Loading ...</p>}>
+        <UserList />
+      </Suspense>
     </main>
   );
 }
