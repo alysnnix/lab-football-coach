e55a3e5dce9b15ef1aa33e1a30d9847c9d26175b
aly <4ly.alcantara@gmail.com>
2025-04-29 02:04:35 -0300

    Initialize project structure for Next.js application (#12)
    * feat: initialize project structure
    
    - add .gitignore to exclude node_modules and build files
    - create eslint.config.mjs for ESLint configuration
    - add next.config.ts for Next.js configuration
    - set up package.json with dependencies and scripts
    - include postcss.config.mjs for PostCSS configuration
    - add SVG files for icons and logos
    - implement global styles in globals.css
    - create layout and page components in src/app
    - configure TypeScript with tsconfig.json
    
    * chore: update project assets and layout
    
    - remove unused SVG files: `file.svg`, `globe.svg`, `next.svg`, `vercel.svg`, `window.svg`
    - add new logo files: `logo.png`, `logo.svg`
    - update layout metadata in `layout.tsx`
    
    * refact: remove unused font imports
    
    - deleted font imports for `Geist` and `Geist_Mono`
    - simplified `body` class in `layout.tsx`
    - cleaned up `globals.css` by removing unused font variables
    
    * chore: update project name in package.json
    
    - changed project name from `teste` to `sensedia-football-coach-challenge`
    
    * Refactor layout and styling components
    
    - Updated globals.css to define new CSS variables for UI elements and added transition properties.
    - Modified layout.tsx to include new font imports and wrap children with Header and Footer components.
    - Changed page.tsx to apply a background color to the main element.
    - Created Footer component with a defined height and background color.
    - Implemented Header component with a logo and navigation elements.
    - Added LogoSVG component with Icon and Text subcomponents for better SVG management.
    - Developed Text component for the logo with specific SVG paths and styles.
    
    * style: update font classes for consistency
    
    - added `font-medoim` and `text-sm` classes to the `Registro` text

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sensedia Football Coach Challenge",
+  description: "Challenge to create a football coach app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased h-full`}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-slate-200">
+      <h1>queijo</h1>
+    </main>
+  );
+}
