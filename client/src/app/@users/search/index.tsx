5f3eff5f217cfd6956879a3f8992430dafddba31
Alysson <4ly.alcantara@gmail.com>
2025-04-30 05:18:13 -0300

    feat(client): integrate nuqs for search functionality
    - added `nuqs` dependency to `package.json`
    - implemented search state management using `useQueryState` from `nuqs`
    - updated search form to handle submissions and display current search value
    - wrapped main layout with `NuqsAdapter` for context

--- client/src/app/@users/search/index.tsx
+++ client/src/app/@users/search/index.tsx
@@ -1,14 +1,32 @@
-import { SearchIcon } from "lucide-react";
+"use client";
+
+import {SearchIcon} from "lucide-react";
+import {useQueryState} from "nuqs";
 
 export const Search = () => {
+  const [search, setSearch] = useQueryState("q", {defaultValue: ""});
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const searchValue = formData.get("search") as string;
+    setSearch(searchValue);
+  };
+
   return (
-    <div className="h-[40px] flex items-center px-3 bg-ui-accent rounded-t-sm border-b border-ui-gray-medium-light">
+    <form
+      onSubmit={handleSubmit}
+      className="h-[40px] flex items-center px-3 bg-ui-accent rounded-t-sm border-b border-ui-gray-medium-light">
       <input
         type="text"
+        name="search"
         placeholder="Buscar"
+        defaultValue={search}
         className="h-full text-md w-full border-none outline-none"
       />
-      <SearchIcon className="text-[#919191] w-[18px] h-[18px]" />
-    </div>
+      <button type="submit" className="cursor-pointer">
+        <SearchIcon className="text-[#919191] w-[18px] h-[18px]" />
+      </button>
+    </form>
   );
 };
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import { Toaster } from "sonner";
+import { NuqsAdapter } from "nuqs/adapters/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,17 +27,17 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        media: '(prefers-color-scheme: dark)',
+        media: "(prefers-color-scheme: dark)",
         url: "/assets/favicon/dark.svg",
         href: "/assets/favicon/dark.svg",
       },
       {
-        media: '(prefers-color-scheme: light)',
+        media: "(prefers-color-scheme: light)",
         url: "/assets/favicon/light.svg",
         href: "/assets/favicon/light.svg",
       },
-    ]
-  }
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -50,11 +51,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased h-full`}>
-        <Toaster />
-        <Header />
-        {children}
-        {users}
-        <Footer />
+        <NuqsAdapter>
+          <Toaster />
+          <Header />
+          {children}
+          {users}
+          <Footer />
+        </NuqsAdapter>
       </body>
     </html>
   );
