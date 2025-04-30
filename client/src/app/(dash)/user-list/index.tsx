b0f6c6793be3bf52d2d025f37c817c7e67254374
Alysson <4ly.alcantara@gmail.com>
2025-04-30 04:17:50 -0300

    feat(client): update user list and table component
    - change `data` type to `GetUsersResponseDto[]` in `UserList`
    - pass `data` prop to `Table` component
    - remove mock user data from `Table`
    - update user fields in `Table` to match API response

--- client/src/app/(dash)/user-list/index.tsx
+++ client/src/app/(dash)/user-list/index.tsx
@@ -1,28 +1,28 @@
 import { Table } from "./table";
 import { Search } from "./search";
 import { Pagination } from "./pagination";
-import { GetUsersResponseDto } from "@/app/api/users/dto";
 import { NotUserFound } from "./not-found";
+import { GetUsersResponseDto } from "@/app/api/users/dto";
 
 export const UserList = async () => {
-  const data: GetUsersResponseDto = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/users")
+  const data: GetUsersResponseDto[] = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/users")
     .then((res) => res.json())
     .catch((err) => {
       console.error("Error fetching users:", err);
     });
 
 
-    if (!data){
-      return (
-        <NotUserFound />
-      )
-    }
+  if (!data) {
+    return (
+      <NotUserFound />
+    )
+  }
 
   return (
     <div className="px-ui-dash py-6 bg-ui-background flex flex-col gap-6">
       <h2 className="text-2xl text-ui-gray-100">Usuarios</h2>
       <Search />
-      <Table />
+      <Table data={data} />
       <Pagination />
     </div>
   );
--- client/src/app/(dash)/user-list/table/index.tsx
+++ client/src/app/(dash)/user-list/table/index.tsx
@@ -1,40 +1,11 @@
 import { cn } from "@/lib/utils";
 import { Delete } from "./delete";
+import { GetUsersResponseDto } from "@/app/api/users/dto";
 
-const users = [
-  {
-    id: 1,
-    user: "Sonya64",
-    nome: "Mildred Turner",
-    email: "Loraine25@hotmail.com",
-    cidade: "Abraham",
-    dias: "Todos",
-    posts: "3",
-    albuns: "2",
-  },
-  {
-    id: 2,
-    user: "selena89",
-    nome: "Troy Boehm",
-    email: "Agnes_Lang@hotmail.com",
-    cidade: "Bahringerbury",
-    dias: "Fim de semana",
-    posts: "3",
-    albuns: "2",
-  },
-  {
-    id: 3,
-    user: "Sonya64",
-    nome: "Mildred Turner",
-    email: "Loraine25@hotmail.com",
-    cidade: "Abraham",
-    dias: "Todos",
-    posts: "3",
-    albuns: "2",
-  },
-];
-
-export const Table = () => {
+type Props = {
+  data: GetUsersResponseDto[];
+}
+export const Table = ({ data }: Props) => {
   return (
     <div className={cn("dash-table overflow-x-auto overflow-y-hidden")}>
       <table className="w-full sm:text-left rounded-sm">
@@ -52,17 +23,23 @@ export const Table = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {data?.map((user) => (
             <tr
               key={user.id}
               className="mb-4 border-b hover:bg-ui-accent text-nowrap">
-              <td className="py-6 px-2 ">{user.user}</td>
-              <td className="py-6 px-2">{user.nome}</td>
-              <td className="py-6 px-2">{user.email}</td>
-              <td className="py-6 px-2">{user.cidade}</td>
-              <td className="py-6 px-2">{user.dias}</td>
+              <td className="py-6 px-2 ">{user.id.split('-')[0]}</td>
+              <td className="py-6 px-2 truncate max-w-[150px]" title={user.name}>
+                {user.name}
+              </td>
+              <td className="py-6 px-2 truncate max-w-[200px]">{user.email}</td>
+              <td className="py-6 px-2 truncate max-w-[150px]" title={user.city}>
+                {user.city}
+              </td>
+              <td className="py-6 px-2 truncate max-w-[150px]" title={user.days_of_week}>
+                {user.days_of_week}
+              </td>
               <td className="py-6 px-2">{user.posts}</td>
-              <td className="py-6 px-2">{user.albuns}</td>
+              <td className="py-6 px-2">{user.albums}</td>
               <Delete />
               <td className="py-6 px-2" />
             </tr>
